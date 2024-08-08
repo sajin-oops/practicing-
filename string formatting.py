cbceea59b9e792dfd3f3ad2c34418ec77ce63e9b
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-08 22:39:08 +0530

    Add files via upload

---
+++ string formatting.py
@@ -0,0 +1,5 @@
+print("***{msg}***".format(msg="Hello Welcome To The Reality"))
+#O/P - ***Hello Welcome To The Reality***
+
+print("*******{:>18}**".format("Hello There"))
+#O/P - *******       Hello There**
